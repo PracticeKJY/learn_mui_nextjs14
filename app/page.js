ba0aab177ec0517b1cd8ff459a2833b57d603357
codespace-jake <jake@codespace.im>
2024-04-12 15:56:16 +0900

    전역 색상 변수 선언

--- app/page.js
+++ app/page.js
@@ -1,17 +1,26 @@
 "use client";
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography, useTheme } from "@mui/material";
 import { styled } from "styled-components";
 
-const Text = styled.div`
-  font-size: 24px;
-  font-weight: 600;
-`;
+const Text = styled("div")(({ theme }) => ({
+  fontSize: 24,
+  fontWeight: 600,
+  color: theme.palette.neutral[25], // 예시로 색상 적용
+}));
+
+// const Text = styled.div`
+//   font-size: 24px;
+//   font-weight: 600;
+// `;
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
     <Container>
       <Typography>세상에 이런 폰트가 나오다니 천재인듯</Typography>
       <Text>세상에 이런 폰트가 나오다니 천재인듯</Text>
+      <Button style={{ backgroundColor: theme.palette.primary[50] }}>My Custom Button</Button>
     </Container>
   );
 }
